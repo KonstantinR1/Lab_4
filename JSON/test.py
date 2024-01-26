03245a60ef9c0b4ec03930928bc5a1b0b3a44bdb
Konstantin Ryaguzov <156826660+KonstantinR1@users.noreply.github.com>
2024-01-26 18:59:11 +0300

    Update test.py

--- JSON/test.py
+++ JSON/test.py
@@ -1,26 +1,6 @@
-import json
+import json  
 
-# Load JSON data from the file
-with open('sample-data.json') as file:
-    data = json.load(file)
-
-# Extract relevant information
-interface_data = data.get('imdata', [])
-
-# Print header
-print("Interface Status")
-print("=" * 80)
-print("{:<50} {:<20} {:<8} {:<6}".format("DN", "Description", "Speed", "MTU"))
-print("-" * 80)
-
-# Print interface details
-for entry in interface_data:
-    l1_phys_if = entry.get('l1PhysIf', {})
-    attributes = l1_phys_if.get('attributes', {})
+with open ("sample-data.json") as json_file:
+    a  = json.load(json_file)
     
-    dn = attributes.get('dn', '')
-    description = attributes.get('descr', '')
-    speed = attributes.get('speed', 'inherit')
-    mtu = attributes.get('mtu', '')
-
-    print("{:<50} {:<20} {:<8} {:<6}".format(dn, description, speed, mtu))
+print(a)
